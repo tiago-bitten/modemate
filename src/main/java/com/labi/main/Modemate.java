69f5039b7cebd1c25c67c42b1459d31529ab19e3
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-08-08 15:39:37 -0300

    feat: added custom craft snow grenade

--- src/main/java/com/labi/main/Modemate.java
+++ src/main/java/com/labi/main/Modemate.java
@@ -1,5 +1,6 @@
 package com.labi.main;
 
+import com.labi.crafts.CustomCrafts;
 import com.labi.listeners.ProjectilesEvents;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -8,9 +9,14 @@ public final class Modemate extends JavaPlugin {
     @Override
     public void onEnable() {
 
+        /* Plugin startup */
         getLogger().info("\u001B[32m" + "Modemate has been enabled!" + "\u001B[0m");
 
+        /* Listeners */
         getServer().getPluginManager().registerEvents(new ProjectilesEvents(), this);
+
+        /* Crafts */
+        CustomCrafts.registerSuperSnowball(this);
     }
 
     @Override
