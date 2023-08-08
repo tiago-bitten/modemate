22637073bee9299b1b9181a73d21512c7173aa04
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-08-08 11:46:01 -0300

    feat: add initial msg on console servere

--- src/main/java/com/labi/main/Modemate.java
+++ src/main/java/com/labi/main/Modemate.java
@@ -6,8 +6,7 @@ public final class Modemate extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getLogger().info("Initialize!!!");
     }
 
     @Override
