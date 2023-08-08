cb8b5b99da19d0a883d1162f290c006a42304c29
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-08-08 14:20:21 -0300

    refactor: remove ProjectileLauchEvent add ProjectileHitEvent

--- src/main/java/com/labi/listeners/PlayerInteractListener.java
+++ src/main/java/com/labi/listeners/PlayerInteractListener.java
@@ -1,25 +1,35 @@
 package com.labi.listeners;
 
+import com.labi.main.Modemate;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.block.data.Snowable;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.event.player.PlayerEggThrowEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 
 public class PlayerInteractListener implements Listener {
 
     @EventHandler
-    public void onSnowballThrow(ProjectileLaunchEvent event) {
+    public void onSnowballThrow(ProjectileHitEvent event) {
         Player player = (Player) event.getEntity().getShooter();
         Projectile projectile = event.getEntity();
 
         if (projectile instanceof Snowball) {
-            player.sendMessage("You threw a snowball!");
+            Block hitBlock = event.getHitBlock();
+
+            if (hitBlock != null ) {
+                hitBlock.getWorld().createExplosion(hitBlock.getLocation(), 2f);
+                projectile.remove();
+            }
         }
     }
 }
