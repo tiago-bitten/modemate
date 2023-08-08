1bcb51ef9a734c0bb48dab7afdeed43bb2b5732c
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-08-08 18:13:09 -0300

    fix: only crafted snow grenades can explode

--- src/main/java/com/labi/listeners/ProjectilesEvents.java
+++ src/main/java/com/labi/listeners/ProjectilesEvents.java
@@ -1,37 +1,50 @@
 package com.labi.listeners;
-
+// ||
+import org.bukkit.ChatColor;
 import org.bukkit.block.Block;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.Projectile;
-import org.bukkit.entity.Snowball;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByBlockEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import static com.labi.listeners.utils.SnowballThrowUtils.randomExplosion;
 
 public class ProjectilesEvents implements Listener {
+
+    private static final String SNOW_GRENADE = ChatColor.YELLOW + "" + ChatColor.BOLD + "" + ChatColor.ITALIC + "Snow Grenade";
 
     @EventHandler
     public void onSnowballThrow(ProjectileHitEvent event) {
         Player player = (Player) event.getEntity().getShooter();
         Projectile projectile = event.getEntity();
 
-        // ||
+        ItemMeta itemMeta = player.getInventory().getItemInMainHand().getItemMeta();
 
-        if (projectile instanceof Snowball) {
-            Block hitBlock = event.getHitBlock();
-            LivingEntity livingEntity = (LivingEntity) event.getHitEntity();
+        if (itemMeta != null && itemMeta.getDisplayName().equals(SNOW_GRENADE)) {
+            if (projectile instanceof Snowball) {
+                Block hitBlock = event.getHitBlock();
+                LivingEntity livingEntity = (LivingEntity) event.getHitEntity();
 
-            if (hitBlock != null ) {
-                hitBlock.getWorld().createExplosion(hitBlock.getLocation(), randomExplosion());
-                projectile.remove();
-            }
+                if (hitBlock != null ) {
+                    hitBlock.getWorld().createExplosion(hitBlock.getLocation(), randomExplosion(), true, true, player);
+                    projectile.remove();
+                }
 
-            if (livingEntity != null) {
-                livingEntity.getWorld().createExplosion(livingEntity.getLocation(), randomExplosion());
-                projectile.remove();
+                if (livingEntity != null) {
+                    double health = livingEntity.getHealth();
+                    livingEntity.setInvulnerable(true);
+
+                    livingEntity.getWorld().createExplosion(livingEntity.getLocation(), randomExplosion(), true, true);
+
+                    livingEntity.setHealth(health);
+                    livingEntity.setInvulnerable(false);
+
+                    projectile.remove();
+                }
             }
         }
     }
