7909bd9b1d043f49b2c35d07f89329648348a136
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-08-09 12:40:23 -0300

    style: method to explode the snowball moved to utils

--- src/main/java/com/labi/listeners/ProjectilesEvents.java
+++ src/main/java/com/labi/listeners/ProjectilesEvents.java
@@ -10,31 +10,29 @@ import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.util.Vector;
 
-import static com.labi.listeners.utils.SnowballThrowUtils.randomExplosion;
+import static com.labi.listeners.utils.SnowballThrowUtils.*;
 
 public class ProjectilesEvents implements Listener {
 
-    private static final String SNOW_GRENADE = ChatColor.YELLOW + "" + ChatColor.BOLD + "" + ChatColor.ITALIC + "Snow Grenade";
-
     @EventHandler
-    public void onSnowballThrow(ProjectileHitEvent event) {
+    public void onSnowballHit(ProjectileHitEvent event) {
         Player player = (Player) event.getEntity().getShooter();
-        Projectile projectile = event.getEntity();
 
+        Projectile projectile = event.getEntity();
         if (!(projectile instanceof Snowball)) return;
 
         ItemMeta itemMeta = player.getInventory().getItemInMainHand().getItemMeta();
+        if (itemMeta == null || !itemMeta.getDisplayName().equals(getSnowGrenadeName())) return;
 
-        if (itemMeta != null && itemMeta.getDisplayName().equals(SNOW_GRENADE)) {
-            Block hitBlock = event.getHitBlock();
-            LivingEntity livingEntity = (LivingEntity) event.getHitEntity();
+        Block hitBlock = event.getHitBlock();
+        LivingEntity livingEntity = (LivingEntity) event.getHitEntity();
 
-            if (hitBlock != null || livingEntity != null) {
-                projectile.getWorld().createExplosion(projectile.getLocation(), randomExplosion(), true, true, player);
-                projectile.remove();
-            }
+        if (hitBlock != null || livingEntity != null) {
+            explodeSnowball(projectile, player);
         }
     }
 }
--- src/main/java/com/labi/listeners/utils/SnowballThrowUtils.java
+++ src/main/java/com/labi/listeners/utils/SnowballThrowUtils.java
@@ -1,11 +1,25 @@
 package com.labi.listeners.utils;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+
 public class SnowballThrowUtils {
 
-    private static final float MIN_EXPLOSION = 1.0f;
-    private static final float MAX_EXPLOSION = 1.5f;
+    private static final float MIN_EXPLOSION = 0.6f;
+    private static final float MAX_EXPLOSION = 1.2f;
+    private static final String SNOW_GRENADE = ChatColor.YELLOW + "" + ChatColor.BOLD + "" + ChatColor.ITALIC + "Snow Grenade";
 
-    public static Float randomExplosion() {
+    public static void explodeSnowball(Projectile projectile, Player player) {
+        projectile.getWorld().createExplosion(projectile.getLocation(), randomExplosion(), true, true, player);
+        projectile.remove();
+    }
+
+    private static Float randomExplosion() {
         return (float) (Math.random() * (MAX_EXPLOSION - MIN_EXPLOSION) + MIN_EXPLOSION);
+    }
+
+    public static String getSnowGrenadeName() {
+        return SNOW_GRENADE;
     }
 }
