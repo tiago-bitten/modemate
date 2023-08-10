f9d6d460e47810877d6516964af7bbadc975424b
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-08-10 15:28:27 -0300

    feat: added tabcomplete in time commands

--- src/main/java/com/labi/commands/TimeCommand.java
+++ src/main/java/com/labi/commands/TimeCommand.java
@@ -4,13 +4,14 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
-public class TimeCommand implements CommandExecutor {
+import java.util.List;
+
+public class TimeCommand implements CommandExecutor, TabCompleter {
 
     private static final String COMMAND_NAME = "mtime";
-    private static final String OPERATOR_PERMISSION = "server.op";
-
     private static final int SUNRISE = 0;
     private static final int MORNING = 1000;
     private static final int NOON = 6000;
@@ -18,6 +19,7 @@ public class TimeCommand implements CommandExecutor {
     private static final int SUNSET = 13000;
     private static final int NIGHT = 14000;
     private static final int MIDNIGHT = 18000;
+    private static final String OPERATOR_PERMISSION = "server.op";
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
@@ -45,37 +47,37 @@ public class TimeCommand implements CommandExecutor {
 
         if (arg.equalsIgnoreCase("sunrise")) {
             player.getWorld().setTime(SUNRISE);
-            player.sendMessage(ChatColor.GREEN + "modemate: " + ChatColor.WHITE + "Time has been set to day!");
+            player.sendMessage(ChatColor.GREEN + "modemate: " + ChatColor.WHITE + "Time has been set to sunrise!");
             return true;
         }
 
         if (arg.equalsIgnoreCase("morning")) {
             player.getWorld().setTime(MORNING);
-            player.sendMessage(ChatColor.GREEN + "modemate: " + ChatColor.WHITE + "Time has been set to night!");
+            player.sendMessage(ChatColor.GREEN + "modemate: " + ChatColor.WHITE + "Time has been set to morning!");
             return true;
         }
 
         if (arg.equalsIgnoreCase("noon")) {
             player.getWorld().setTime(NOON);
-            player.sendMessage(ChatColor.GREEN + "modemate: " + ChatColor.WHITE + "Time has been set to midnight!");
+            player.sendMessage(ChatColor.GREEN + "modemate: " + ChatColor.WHITE + "Time has been set to noon!");
             return true;
         }
 
         if (arg.equalsIgnoreCase("afternoon")) {
             player.getWorld().setTime(AFTERNOON);
-            player.sendMessage(ChatColor.GREEN + "modemate: " + ChatColor.WHITE + "Time has been set to noon!");
+            player.sendMessage(ChatColor.GREEN + "modemate: " + ChatColor.WHITE + "Time has been set to afternoon!");
             return true;
         }
 
         if (arg.equalsIgnoreCase("sunset")) {
             player.getWorld().setTime(SUNSET);
-            player.sendMessage(ChatColor.GREEN + "modemate: " + ChatColor.WHITE + "Time has been set to afternoon!");
+            player.sendMessage(ChatColor.GREEN + "modemate: " + ChatColor.WHITE + "Time has been set to sunset!");
             return true;
         }
 
         if (arg.equalsIgnoreCase("night")) {
             player.getWorld().setTime(NIGHT);
-            player.sendMessage(ChatColor.GREEN + "modemate: " + ChatColor.WHITE + "Time has been set to morning!");
+            player.sendMessage(ChatColor.GREEN + "modemate: " + ChatColor.WHITE + "Time has been set to night!");
             return true;
         }
 
@@ -84,13 +86,16 @@ public class TimeCommand implements CommandExecutor {
             player.sendMessage(ChatColor.GREEN + "modemate: " + ChatColor.WHITE + "Time has been set to midnight!");
             return true;
         }
-
-        if (arg.equalsIgnoreCase("sun")) {
-            player.getWorld().setStorm(false);
-            player.sendMessage(ChatColor.GREEN + "modemate: " + ChatColor.WHITE + "Weather has been set to sun!");
-            return true;
-        }
         return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
+        List<String> args = List.of("sunrise", "morning", "noon", "afternoon", "sunset", "night", "midnight");
+        if (strings.length == 1) {
+            return args;
+        }
+        return null;
     }
 
     public static String getCommandName() {
