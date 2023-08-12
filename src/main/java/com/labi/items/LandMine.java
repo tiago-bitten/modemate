1ff17ec5ce0e3cddf01352af045ca1665d33cbde
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-08-12 00:35:51 -0300

    feat: created land mine item class

---
+++ src/main/java/com/labi/items/LandMine.java
@@ -0,0 +1,53 @@
+package com.labi.items;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
+import java.util.UUID;
+
+public class LandMine {
+
+    private static final UUID ITEM_UUID = UUID.fromString("a3f7b2e0-4e0a-11eb-ae93-0242ac130002");
+    private static final String ITEM_NAME = ChatColor.YELLOW + "" + ChatColor.BOLD + "" + ChatColor.ITALIC + "Land Mine";
+    private static final String ITEM_LORE = ChatColor.GRAY + "Place this item on the ground to create a land mine!";
+
+    public static ItemStack createLandMine() {
+        ItemStack landMine = new ItemStack(Material.STONE_PRESSURE_PLATE);
+        ItemMeta landMineMeta = landMine.getItemMeta();
+
+        landMineMeta.setDisplayName(ITEM_NAME);
+        landMineMeta.setLore(Arrays.asList(ITEM_LORE));
+        landMineMeta.setCustomModelData(ITEM_UUID.hashCode());
+        landMineMeta.addEnchant(Enchantment.DURABILITY, 1, true);
+        landMineMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+
+        landMine.setItemMeta(landMineMeta);
+
+        return landMine;
+    }
+
+    public static boolean isLandMine(ItemStack item) {
+        if (item == null) return false;
+        if (!item.hasItemMeta()) return false;
+        if (!item.getItemMeta().hasCustomModelData()) return false;
+
+        return item.getItemMeta().getCustomModelData() == ITEM_UUID.hashCode();
+    }
+
+    public static UUID getItemUUID() {
+        return ITEM_UUID;
+    }
+
+    public static String getItemName() {
+        return ITEM_NAME;
+    }
+
+    public static String getItemLore() {
+        return ITEM_LORE;
+    }
+}
