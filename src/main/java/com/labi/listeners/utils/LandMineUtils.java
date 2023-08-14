b6b39e9011f6aea686ef0968b7a1c41a0c5b4ad9
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-08-13 23:58:00 -0300

    feat: created interface for explosions

--- src/main/java/com/labi/listeners/utils/LandMineUtils.java
+++ src/main/java/com/labi/listeners/utils/LandMineUtils.java
@@ -1,6 +1,16 @@
 package com.labi.listeners.utils;
 
-public class LandMineUtils {
+import com.labi.utils.ExplodeItem;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 
+public class LandMineUtils implements ExplodeItem<Block, Player> {
 
+    private static final int EXPLOSION_RANGE = 5;
+
+    @Override
+    public void explode(Block obj, Player reference) {
+        obj.getWorld().createExplosion(obj.getLocation(), EXPLOSION_RANGE, true, true, reference);
+        reference.damage(10);
+    }
 }
---
+++ src/main/java/com/labi/utils/ExplodeItem.java
@@ -0,0 +1,6 @@
+package com.labi.utils;
+
+public interface ExplodeItem<T, K> {
+
+    void explode(T obj, K reference);
+}
