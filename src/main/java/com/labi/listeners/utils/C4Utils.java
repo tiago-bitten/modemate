de4a152272d24e1ebb5722473bd1fbba7a1a41bc
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-08-14 12:43:11 -0300

    feat: added sound when starting c4 counting

--- src/main/java/com/labi/listeners/utils/C4Utils.java
+++ src/main/java/com/labi/listeners/utils/C4Utils.java
@@ -3,6 +3,7 @@ package com.labi.listeners.utils;
 import com.labi.main.Modemate;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
@@ -19,10 +20,10 @@ public class C4Utils {
             c4.getWorld().createExplosion(c4.getLocation(), 4.0F, true, true, player);
         }, C4_COOLDOWN_SECONDS * 20L);
 
-        startCountdown(player);
+        startCountdown(c4, player);
     }
 
-    private static void startCountdown(Player player) {
+    private static void startCountdown(Block c4, Player player) {
         final int[] countdownTime = {C4_COOLDOWN_SECONDS};
 
         if (timerStarted) return;
@@ -31,6 +32,7 @@ public class C4Utils {
         Bukkit.getScheduler().runTaskTimer(MODEMATE, () -> {
             if (countdownTime[0] > 0) {
                 player.sendMessage(ChatColor.RED + "C4 will explode in " + ChatColor.YELLOW + countdownTime[0] + ChatColor.RED + " seconds!");
+                playC4Sound(c4);
                 countdownTime[0]--;
             } else {
                 player.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "C4 exploded!");
@@ -38,5 +40,9 @@ public class C4Utils {
                 Bukkit.getScheduler().cancelTasks(MODEMATE);
             }
         }, 0L, 20L);
+    }
+
+    private static void playC4Sound(Block c4) {
+        c4.getWorld().playSound(c4.getLocation(), Sound.ITEM_LODESTONE_COMPASS_LOCK, 1.0F, 1.0F);
     }
 }
