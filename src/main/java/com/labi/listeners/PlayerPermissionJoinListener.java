b0faa5ba24adf86dc5d582b36cdc534e516b879a
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-08-15 23:11:40 -0300

    style: changed class name to cover any player that might have any permissions

--- src/main/java/com/labi/listeners/PlayerPermissionJoinListener.java
+++ src/main/java/com/labi/listeners/PlayerPermissionJoinListener.java
@@ -1,7 +1,6 @@
 package com.labi.listeners;
 
 import com.labi.commands.ModemateCommand;
-import com.labi.main.Modemate;
 import com.labi.permissions.PermissionsEnum;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -9,19 +8,19 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class OperatorPlayerJoinServer implements Listener {
+public class PlayerPermissionJoinListener implements Listener {
 
     private PermissionsEnum permissionsEnum;
 
     private JavaPlugin modemate;
     private ModemateCommand modemateCommand;
 
-    public OperatorPlayerJoinServer(JavaPlugin modemate, ModemateCommand modemateCommand) {
+    public PlayerPermissionJoinListener(JavaPlugin modemate, ModemateCommand modemateCommand) {
         this.modemate = modemate;
         this.modemateCommand = modemateCommand;
     }
     @EventHandler
-    public void onPlayerJoinSever(PlayerJoinEvent event) {
+    public void onOperatorJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
 
         if (!player.hasPermission(permissionsEnum.OPERATOR.getPermission())) return;
