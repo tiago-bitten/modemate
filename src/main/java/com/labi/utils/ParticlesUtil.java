ced15acbfe1547b451434fb8052fac1a17e68c3d
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-08-17 15:27:48 -0300

    feat: created utils class for particles

--- src/main/java/com/labi/utils/ParticlesUtil.java
+++ src/main/java/com/labi/utils/ParticlesUtil.java
@@ -1,15 +1,26 @@
 package com.labi.utils;
 
+import com.labi.main.Modemate;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.World;
 
 public class ParticlesUtil {
 
-    public static void applyParticleEffect(Location location, Particle particle, int amount, int radius) {
+    public static void applyParticleEffect(Location location, Particle particle, int amount, int radius, int speed) {
         World world = location.getWorld();
         if (world == null) return;
 
-        world.spawnParticle(particle, location, amount, radius, radius, radius);
+        world.spawnParticle(particle, location, amount, radius, radius, radius, speed);
+    }
+
+    public static void createParticleTrailEffect(Location location, Particle particle, int amount, int radius, int speed, int ticks) {
+        World world = location.getWorld();
+        if (world == null) return;
+
+        Bukkit.getScheduler().runTaskTimer(Modemate.getInstance(), () -> {
+            applyParticleEffect(location, particle, amount, radius, speed);
+        }, 0L, ticks);
     }
 }
